7fe5748b6df5e4aba3eb075941ac07d834881148
ield <nachol2013@gmail.com>
2020-09-25 17:35:00 +0200

    New modifications in scripts

--- CircularPatchDimensions.m
+++ CircularPatchDimensions.m
@@ -4,14 +4,24 @@
 clear all;
 
 % Modify these values
-f = 1e9;                  % Central frequency [Hz]
-e = 2.2;                  % Dielectric constant
-h = 0.2e-3;               % Thickness of the substrate [m]
+f = 3.4e9;                % Central frequency [Hz]
+e = 2.33;                 % Dielectric constant
+h = 0.7e-3;                  % Thickness of the substrate [m] (probably)
 Rin = 50;                 % Input resistance at feeding point [ohm]
 
 c = 3e8;                % Speed of light
 n0 = 120*pi;            % Vacuum impedance
 
+% First approximation of L (the gnd and substrate length)
+lambda0 = c / f;                % Wavelength
+L = lambda0 / 2                 % Length of the substrate [m]
+
+
 % First approximation of r (the patch length) taken from diapo 29
 F = 8.794e9/(f*sqrt(e));
-r = F / sqrt(1+2*h/(pi*e*F)*(log(pi*F/(2*h))+1.7726)) % Radius of the patch [cm]
+% r = (F / sqrt(1+2*h/(pi*e*F)*(log(pi*F/(2*h))+1.7726)))/100 % Radius of the patch [m]
+r = 15.9e-3;
+
+% Feeding point: set for Rin = 50m ohm
+R0 = n0*lambda0 / (2*pi*2*r*(1-(1/6)*(pi*h/lambda0)^2));
+L1 = 2*r/pi*acos(sqrt(Rin / R0)) % Feeding point        
--- SqarePatchDimensions.m
+++ SqarePatchDimensions.m
@@ -4,10 +4,9 @@
 clear all;
 
 % Modify these values
-f = 3.5e9;                % Central frequency [Hz]
+f = 2.4e9;                % Central frequency [Hz]
 e = 2.33;                 % Dielectric constant
-% h = 0.2e-3;               % Thickness of the substrate [m]: when given comment line 10, 26, 30
-bw = 200e6;               % Bandwidth [MHz]: when given, comment line 9, 25
+h = 0.7e-3;               % Thickness of the substrate [m]: when given comment line 10, 26, 30
 Rin = 50;                 % Input resistance at feeding point [ohm]
 
 c = 3e8;                % Speed of light
@@ -22,11 +21,6 @@ L = lambda0 / 2                 % Length of the substrate [m]
 % Feeding point: set for Rin = 50m ohm
 %Using the formulas in page 27, first it is needed to know the thickness
 %Using the formula in page 11
-% bw = 16/(3*sqrt(2))*1/e*h/lambda0 % Bandwidth of the substrate. Since the patch is a square, w/L = 1. Formula taken from diapo 11.
-bw = bw / 1e10;                     % The bw is transformed to the appropriate units. Conversion obtained empirically
-h = bw*3*sqrt(2)/16*e*lambda0       % Thickness of the subtrate [m]
-R0 = n0*lambda0 / (2*pi*bw*(1-(1/6)*(pi*h/lambda0)^2));
-L1 = Lpatch/pi*acos(sqrt(Rin / R0)) % Feeding point         
-
-% Othe parameters 
-% eEf = (e+1) / 2 + (e - 1) / (2 * sqrt(1 + 12 * h / L));
+R0 = n0*lambda0 / (2*pi*Lpatch*(1-(1/6)*(pi*h/lambda0)^2));
+L1 = Lpatch/pi*acos(sqrt(Rin / R0)) % Feeding point   
+Lpos = Lpatch/2-L1
